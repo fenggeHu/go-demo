4a804a8fcc38f61800a93707d82e2c0ce5fb85fb
hujinfeng <pbfoxhu@gmail.com>
2022-01-08 13:07:53 +0800

    后台服务

--- admin/admin_server.go
+++ admin/admin_server.go
@@ -5,7 +5,7 @@ import (
 	"jinfeng.hu/goapi/admin/controller"
 )
 
-// Server 后台服务
+// Server 后台服务 - 分组
 func Server() {
 	router := gin.Default()
 	// 设置一个get请求的路由，url为/ping, 处理函数（或者叫控制器函数）是一个闭包函数。
@@ -16,7 +16,7 @@ func Server() {
 		})
 	})
 
-	user :=router.Group("/user/v1")
+	user := router.Group("/user/v1")
 	{
 		// user login
 		user.POST("/login", controller.Login)
